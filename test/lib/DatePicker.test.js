8eb6ddcb71dc74b277d0f828dc733edfaa2577b8
Bibek Ghimire <bibekg@uber.com>
2018-06-27 12:13:39 -0700

    Add a date cell render prop test

--- test/lib/DatePicker.test.js
+++ test/lib/DatePicker.test.js
@@ -1,16 +1,34 @@
+// @flow
+
 import React from 'react'
 import renderer from 'react-test-renderer'
 import moment from 'moment'
 import DatePicker from '../../src/lib/DatePicker'
 
-test('Component renders correctly', () => {
-  // Super basic test of whether it renders correctly
-  const testSchedule = [
-    moment().startOf('day').add(12, 'h'),
-    moment().startOf('day').add(1, 'd').add(13, 'h')
-  ]
+const getTestSchedule = (): Array<moment> => [
+  moment()
+    .startOf('day')
+    .add(12, 'h'),
+  moment()
+    .startOf('day')
+    .add(1, 'd')
+    .add(13, 'h')
+]
 
-  const component = renderer.create(<DatePicker selection={testSchedule} onChange={() => undefined} />)
+test('Component renders correctly', () => {
+  const component = renderer.create(<DatePicker selection={getTestSchedule()} onChange={() => undefined} />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Date cell render prop is used if provided', () => {
+  const customDateCellRenderer = (date: Date, selected: boolean): React.Element<*> => (
+    <div className={`${selected && 'selected'} test-date-cell-renderer`}>{date.toDateString()}</div>
+  )
+
+  const component = renderer.create(
+    <DatePicker selection={getTestSchedule()} onChange={() => undefined} renderDateCell={customDateCellRenderer} />
+  )
 
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
