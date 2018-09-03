c5e9a3c34d4ec72cfeba4825c5016f043176b4d1
Bibek Ghimire <ghim.bibek@gmail.com>
2018-09-02 17:40:35 -0700

    Add travis config (#5)
    * Add travis config
    
    * Fix yarn lock
    
    * Fix test timing
    
    * Remove zero offset specifier
    
    * Add coveralls config
    
    * Add coveralls command
    
    * Whoops, forgot coveralls
    
    * ignore coverage folder
    
    * Add coverage badge
    
    * Cleanup

--- test/lib/DatePicker.test.js
+++ test/lib/DatePicker.test.js
@@ -1,33 +1,41 @@
-// @flow
-
 import React from 'react'
 import renderer from 'react-test-renderer'
 import moment from 'moment'
 import DatePicker from '../../src/lib/DatePicker'
 
-const getTestSchedule = (): Array<moment> => [
-  moment()
+const startDate = new Date('2018-01-01T00:00:00.000')
+
+const getTestSchedule = () => [
+  moment(startDate)
     .startOf('day')
     .add(12, 'h'),
-  moment()
+  moment(startDate)
     .startOf('day')
     .add(1, 'd')
     .add(13, 'h')
 ]
 
 test('Component renders correctly', () => {
-  const component = renderer.create(<DatePicker selection={getTestSchedule()} onChange={() => undefined} />)
+  const component = renderer.create(
+    <DatePicker selection={getTestSchedule()} startDate={startDate} numDays={5} onChange={() => undefined} />
+  )
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 test('Date cell render prop is used if provided', () => {
-  const customDateCellRenderer = (date: Date, selected: boolean): React.Element<*> => (
+  const customDateCellRenderer = (date, selected) => (
     <div className={`${selected && 'selected'} test-date-cell-renderer`}>{date.toDateString()}</div>
   )
 
   const component = renderer.create(
-    <DatePicker selection={getTestSchedule()} onChange={() => undefined} renderDateCell={customDateCellRenderer} />
+    <DatePicker
+      selection={getTestSchedule()}
+      startDate={startDate}
+      numDays={5}
+      onChange={() => undefined}
+      renderDateCell={customDateCellRenderer}
+    />
   )
 
   const tree = component.toJSON()
