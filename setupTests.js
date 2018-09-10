4e9c3b588bad0a7d73e5824763a5f68f462e119e
Bibek <ghim.bibek@gmail.com>
2018-09-10 14:56:40 -0700

    Use jest-styled-components for better testing

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+import 'jest-styled-components'
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() })
