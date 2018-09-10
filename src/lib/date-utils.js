82cc1e95b2db5458f2c3e7274d297693c8f7aaa5
Bibek <ghim.bibek@gmail.com>
2018-09-10 13:28:53 -0700

    Add date util tests

--- src/lib/date-utils.js
+++ src/lib/date-utils.js
@@ -1,12 +1,12 @@
 // @flow
 
-import differenceInHours from 'date-fns/difference_in_hours'
 import startOfDay from 'date-fns/start_of_day'
 import isAfter from 'date-fns/is_after'
 
 // Helper function that uses date-fns methods to determine if a date is between two other dates
-export const dateHourIsBetween = (start: Date, candidate: Date, end: Date) =>
-  differenceInHours(candidate, start) >= 0 && differenceInHours(end, candidate) >= 0
+export const dateHourIsBetween = (start: Date, candidate: Date, end: Date): boolean =>
+  (candidate.getTime() === start.getTime() || isAfter(candidate, start)) &&
+  (candidate.getTime() === end.getTime() || isAfter(end, candidate))
 
 export const dateIsBetween = (start: Date, candidate: Date, end: Date): boolean => {
   const startOfCandidate = startOfDay(candidate)
