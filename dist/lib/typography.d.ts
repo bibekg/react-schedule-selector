8ec42f1c085289710c3f4d1a9950f2ce7a5d9706
Mike-Vega <56933662+Mike-Vega@users.noreply.github.com>
2023-06-19 10:46:28 -0700

    bump

---
+++ dist/lib/typography.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+export declare const Subtitle: import("@emotion/styled-base").StyledComponent<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>, {
+    align?: string | undefined;
+}, object>;
+export declare const Text: import("@emotion/styled-base").StyledComponent<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>, Pick<import("react").ClassAttributes<HTMLParagraphElement> & import("react").HTMLAttributes<HTMLParagraphElement>, keyof import("react").HTMLAttributes<HTMLParagraphElement>>, object>;
+//# sourceMappingURL=typography.d.ts.map
